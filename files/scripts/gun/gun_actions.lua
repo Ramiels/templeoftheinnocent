60f0dfcc06929c077dede02f4de084c82c9cf9c6
Graham <catface238@gmail.com>
2022-10-28 23:54:37 -0400

    Added the Caller Shot and related files

--- files/scripts/gun/gun_actions.lua
+++ files/scripts/gun/gun_actions.lua
@@ -16,6 +16,23 @@ local to_insert = {
             add_projectile("mods/templeoftheinnocent/files/entities/projectiles/ball.xml")
         end,
     },
+    {
+        id                  = "COWARD_SHOT",
+        name                = "$action_name_toti_cowardshot",
+        description         = "$action_desc_toti_cowardshot",
+        author              = "Graham",
+        sprite              = "mods/templeoftheinnocent/files/ui_gfx/gun_actions/cowardshot.png",
+        related_projectiles = { "data/entities/projectiles/coward_bullet.xml" },
+        type                = ACTION_TYPE_PROJECTILE,
+        spawn_level         = "0",
+        spawn_probability   = "0",
+        price               = 160,
+        mana                = 100,
+        max_uses            = -1,
+        action              = function()
+            add_projectile("data/entities/projectiles/coward_bullet.xml")
+        end,
+    },
 }
 
 for _, value in ipairs(to_insert) do
